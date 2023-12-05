076dd112e6e04f859893af443375cecab1faf79a
Matt Thompson <matthewthompson118@gmail.com>
2023-12-05 15:47:48 -0400

    added more html,css and js

---
+++ script.js
@@ -0,0 +1,27 @@
+const quizData = [
+    {
+        question: "JavaScript is an _______ language?",
+        options: ["Object-Oriented", "Object-Based", "Procedural", "None of the above"],
+        correctAnswer: "Object-Oriented"
+    },
+    {
+        question: "Which of the following keywords is used to define a variable in JavaScript?",
+        options: ["var", "let", "Both A and B", "None of the above"],
+        correctAnswer: "Both A and B"
+    },
+    {
+        question: "How can a datatype be declared to be a constant type?",
+        options: ["const", "var", "let", "All of the Above"],
+        correctAnswer: "const"
+    },
+    {
+        question: "How to stop an interval timer in JavaScript?",
+        options: ["clearInterval", "clearTimer", "intervalOver", "None of the above"],
+        correctAnswer: "clearInterval" 
+    },
+    {
+        question: "How do we write comments in JavaScript?",
+        options: ["/**/", "//", "#", "$$"],
+        answer: "//"
+    },
+];
